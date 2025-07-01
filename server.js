5319f12631b1f573708c4ee71a64ea4517fcece1
Glitch (glitch-blank-node) <none>
2025-07-01 01:51:18 +0000

    🏙🚇 Checkpoint ./server.js:82058344/113 ./package.json:82058344/69

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ app.get("/", (req, res) => {
   res.send("Hola Mundo");
 });
 
+const fnHealth = (req, res) => {
+  res.send("OK");
+}
+
+app.get("/health", fnHealth);
+
 app.listen(process.env.PORT, () => {
   console.log("El proyecto Express funciona!");
 });
